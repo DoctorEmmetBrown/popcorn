2f35c7bbaf23d02928a7635679a5a86e79f414d8
TAVAKOLIClement <clement.tavakoli@gmail.com>
2021-03-03 16:19:36 +0100

    PEP8 comments and formatting

--- Material.py
+++ Material.py
@@ -5,134 +5,115 @@ import numpy as np
 
 class Material:
     def __init__(self, name):
+        """Material class constructor.
+
+        :param name: material's name
+        :type name: string
+
+        Notes:
+            If the material doesn't correspond to any existing material, the user will be asked to create one.
         """
-        Material class constructor
-            Args:
-                name (string):   material's name
-
-            Returns:
-                None
-
-            Notes:
-                If the material doesn't correspond to any existing material, the user will be asked to create one.
-
-            Examples:
-                >>> gold = Material("Gold")
-            """
         self.name = name
+        # If the material exists, we retrieve its formula and density
         if xraydb.find_material(name):
-            self.chemicalFormula, self.density = xraydb.get_material(name)
+            self.chemical_formula, self.density = xraydb.get_material(name)
+        # If the material doesn't exist, we ask the user to create a new one
         else:
-            userMaterialCreation = input("Material " + str(name) + " doesn't seem to exist, do you want to create it ? [Y/N]: ")
-            if "y" in userMaterialCreation.lower():
-                userChemicalFormula = input("Please enter the chemical formula for your new material.\n >Example: Au0.55Xe0.35Gd0.1\n")
-                while not xraydb.validate_formula(userChemicalFormula):
-                    userChemicalFormula = input("Chemical formula not valid.\n >Example: Au0.55Xe0.35Gd0.1\n")
-                userDensity = float(input("Please enter the material's density: "))
-                xraydb.add_material(name, userChemicalFormula, userDensity)
-                self.chemicalFormula = userChemicalFormula
-                self.density = userDensity
+            # Asking the user if he wants to create a new material
+            user_material_creation = input(
+                "Material " + str(name) + " doesn't seem to exist, do you want to create it ? [Y/N]: ")
+            if "y" in user_material_creation.lower():
+                # We ask the user to give the chemical formula with the proper format
+                user_chemical_formula = input(
+                    "Please enter the chemical formula for your new material.\n >Example: Au0.55Xe0.35Gd0.1\n")
+                while not xraydb.validate_formula(user_chemical_formula):
+                    user_chemical_formula = input("Chemical formula not valid.\n >Example: Au0.55Xe0.35Gd0.1\n")
+                # Once the given formula is valid, we ask the user to give the materials density
+                user_density = float(input("Please enter the material's density: "))
+
+                # We add the material to the materials list
+                xraydb.add_material(name, user_chemical_formula, user_density)
+
+                # We initialize the formula and density attributes
+                self.chemical_formula = user_chemical_formula
+                self.density = user_density
+
+            # If the user doesn't want to create a new material
             else:
                 sys.exit("Please try again with an existing material formula.")
 
+    def get_mu(self, energy, density=None):
+        """linear attenuation of the material at given energies
 
-    def getMu(self, energies, density=None):
-        """Linear attenuation of the material at given energies
-
-            Args:
-                energies (float or ndarray): energy or array of energies in eV
-                density (None or float):   material density (g/cm^3).
-            Returns:
-                Absorption length in 1/cm
-            Notes:
-                If density is None and material is known, that density will be used.
-
-            Examples:
-                >>> gold.getMu(10000.0)
-                2279.92920598
-            """
-        return xraydb.material_mu(self.chemicalFormula, energies, density)
-
-
-    def getBeta(self, energy, density):
+        :param energy: energy or array of energies in eV
+        :type energy: float | numpy.ndarray
+        :param density: material density (g/cm^3)
+        :type density: None or float
+        :return: absorption length in 1/cm
+        :rtype: float | numpy.ndarray
         """
-        Beta coefficient of the material of given density at given energy
-            Args:
-                energy (float): energy in eV
-                density (float): material density (g/cm^3).
+        return xraydb.material_mu(self.chemical_formula, energy, density)
 
-            Returns:
-                Beta coefficient
+    def get_beta(self, energy, density):
+        """beta coefficient of the material of given density at given energy
 
-            Examples:
-                >>> gold.getBeta(10000.0, 19.3)
-                2.084575845737037e-06
-            """
-        return xraydb.xray_delta_beta(self.chemicalFormula, density, energy)[1]
-
-
-    def getDelta(self, energy, density):
+        :param energy: energy in eV
+        :type energy: float
+        :param density: material density (g/cm^3)
+        :type density: float
+        :return: beta coefficient
+        :rtype: float
         """
-        Beta coefficient of the material of given density at given energy
-            Args:
-                energy (float): energy in eV
-                density (float): material density (g/cm^3).
+        return xraydb.xray_delta_beta(self.chemical_formula, density, energy)[1]
 
-            Returns:
-                Beta coefficient
+    def get_delta(self, energy, density):
+        """delta coefficient of the material of given density at given energy
 
-            Examples:
-                >>> gold.getBeta(10000.0, 19.3)
-                2.084575845737037e-06
-            """
-        return xraydb.xray_delta_beta(self.chemicalFormula, density, energy)[0]
-
-
-    def getBetaFromSpectrum(self, energySpectrum, density):
+        :param energy: energy in eV
+        :type energy: float
+        :param density: material density (g/cm^3)
+        :type density: float
+        :return: delta coefficient
+        :rtype: float
         """
-        Beta coefficient of the material of given density at given energies
-            Args:
-                energySpectrum (ndarray of float): array of energies in eV
-                density (float): material density (g/cm^3).
+        return xraydb.xray_delta_beta(self.chemical_formula, density, energy)[0]
 
-            Returns:
-                Beta coefficients for each given energy
+    def get_beta_from_spectrum(self, energy_spectrum, density):
+        """beta coefficient of the material of given density at given energies
 
-            Examples:
-                >>> gold.getBetaFromSpectrum(np.array([10000, 35000, 40000, 45000, 50000]), 19.3)
-                [2.084575845737037e-06, 9.090995289307478e-08, 5.519117987481352e-08, 3.5635111431158425e-08, 2.4144113225644783e-08]
-            """
-        listOfBeta = []
-        for energy in energySpectrum:
-            listOfBeta.append(self.getBeta(int(energy), density))
-        return listOfBeta
-
-
-    def getDeltaFromSpectrum(self, energySpectrum, density):
+        :param energy_spectrum: array of energies in eV
+        :type energy_spectrum: numpy.ndarray
+        :param density: material density (g/cm^3)
+        :type density: float
+        :return: beta coefficients for each given energy
+        :rtype: float
         """
-        Delta coefficient of the material of given density at given energies
-            Args:
-                energySpectrum (ndarray of float): array of energies in eV
-                density (float): material density (g/cm^3).
+        list_of_betas = []
+        for energy in energy_spectrum:
+            list_of_betas.append(self.get_beta(int(energy), density))
+        return list_of_betas
 
-            Returns:
-                Delta coefficients for each given energy
+    def get_delta_from_spectrum(self, energy_spectrum, density):
+        """delta coefficient of the material of given density at given energies
 
-            Examples:
-                >>> gold.getDeltaFromSpectrum(np.array([10000, 35000, 40000, 45000, 50000]), 19.3)
-                [2.970837201294633e-05, 2.5979065733569894e-06, 1.9844421586969203e-06, 1.564375753386381e-06, 1.2643572704001252e-06]
-            """
-        listOfDelta = []
-        for energy in energySpectrum:
-            listOfDelta.append(self.getDelta(int(energy), density))
-        return listOfDelta
+        :param energy_spectrum: array of energies in eV
+        :type energy_spectrum: numpy.ndarray
+        :param density: material density (g/cm^3)
+        :type density: float
+        :return: delta coefficients for each given energy
+        :rtype: float
+        """
+        list_of_deltas = []
+        for energy in energy_spectrum:
+            list_of_deltas.append(self.get_delta(int(energy), density))
+        return list_of_deltas
 
 
 if __name__ == '__main__':
     gold = Material("Gold")
     energies = np.array([10000, 35000, 40000, 45000, 50000])
-    print("Mus :", gold.getMu(energies))
-    print("Beta :", gold.getBeta(int(energies[0]), 19.3))
-    print("Betas :", gold.getBetaFromSpectrum(energies, 19.3))
-    print("Delta :", gold.getDelta(int(energies[0]), 19.3))
-    print("Deltas :", gold.getDeltaFromSpectrum(energies, 19.3))
+    print("Mus :", gold.get_mu(energies))
+    print("Beta :", gold.get_beta(int(energies[0]), 19.3))
+    print("Betas :", gold.get_beta_from_spectrum(energies, 19.3))
+    print("Delta :", gold.get_delta(int(energies[0]), 19.3))
+    print("Deltas :", gold.get_delta_from_spectrum(energies, 19.3))
